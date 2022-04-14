6626149a0c01f32dc86ded2a8f2853b24674ed34
marco <marquinco@outlook.com>
2022-04-14 11:29:53 -0300

    Feat: Reimplementa pagina da agenda

--- client/src/pages/Agenda.js
+++ client/src/pages/Agenda.js
@@ -1,10 +1,23 @@
-import React from 'react';
-// import Context from '../context/Context';
+import React, { useContext, useEffect, useState } from 'react';
+import Context from '../context/Context';
 
 export default function Agenda() {
+  const [contacts, setContacts] = useState([]);
+  const { token } = useContext(Context);
+  useEffect(async () => {
+    if (token) {
+      const fetchData = await fetch('http://localhost:3001/user', { headers: { Authorization: `Bearer ${token}` } });
+      const jsonData = await fetchData.json();
+      setContacts(jsonData);
+      if (jsonData.message) {
+        window.alert(jsonData.message);
+      }
+    }
+  }, []);
   return (
     <div>
       <div>Agenda</div>
+      {contacts.map((contact) => (<p>{contact.name}</p>))}
     </div>
   );
 }
