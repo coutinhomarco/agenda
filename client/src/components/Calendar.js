67e810742a5e0ffbd35b186de82879378e6a858f
marco <marquinco@outlook.com>
2022-06-06 19:18:37 -0300

    feat: add datepicker component

--- client/src/components/Calendar.js
+++ client/src/components/Calendar.js
@@ -1,15 +1,26 @@
 import React from 'react';
 import FullCalendar from '@fullcalendar/react';
 import dayGridPlugin from '@fullcalendar/daygrid';
+import timeGridPlugin from '@fullcalendar/timegrid';
+import interactionPlugin from '@fullcalendar/interaction';
+import ptBrLocale from '@fullcalendar/core/locales/pt-br';
 
 export default function Calendar() {
   return (
     <section className="tasks-main">
       <FullCalendar
         className="tasks-calendar"
-        plugins={[dayGridPlugin]}
-        initialView="dayGridMonth"
+        locales={[ptBrLocale]}
+        headerToolbar={{
+          left: 'prev,next,today',
+          center: 'title',
+          right: 'dayGridMonth,timeGridWeek,timeGridDay',
+        }}
+        plugins={[dayGridPlugin, timeGridPlugin, interactionPlugin]}
+        // events={taskList}
+        dateClick={(e) => console.log(e)}
       />
+
     </section>
   );
 }
--- client/src/pages/Tasks.js
+++ client/src/pages/Tasks.js
@@ -1,11 +1,19 @@
 import React, { useEffect, useContext } from 'react';
+import DatePicker, { registerLocale } from 'react-datepicker';
+import ptBR from 'date-fns/locale/pt-BR';
 import Calendar from '../components/Calendar';
 import Context from '../context/Context';
+
+import 'react-datepicker/dist/react-datepicker.css';
+
+registerLocale('pt-BR', ptBR);
 
 export default function Tasks() {
   const {
     userDetails, setUserDetails,
   } = useContext(Context);
+
+  //   const [startDate, setStartDate] = useState(new Date());
 
   useEffect(() => {
     if (!userDetails.name) {
@@ -23,6 +31,17 @@ export default function Tasks() {
         </h1>
         <button className="btn btn-danger" type="button">Log out</button>
       </header>
+      <DatePicker
+        className="w-full"
+        // selected={displayDate || new Date()}
+        // onChange={(date) => handleChange(date)}
+        showTimeSelect
+        locale="pt-BR"
+        timeIntervals={15}
+        dateFormat="Pp"
+        placeholderText="Selecione uma data..."
+        required
+      />
       <Calendar />
     </div>
   );
