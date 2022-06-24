ceea4320c2792e601e56ee4bb4eac00db47b34a7
marco <marquinco@outlook.com>
2022-06-24 13:41:50 -0300

    change the sequelize config back to env

--- server/sequelize/config/config.js
+++ server/sequelize/config/config.js
@@ -4,22 +4,25 @@ module.exports = {
   development: {
     username: process.env.MYSQL_USER,
     password: process.env.MYSQL_PASSWORD,
-    database: process.env.MYSQL_DATABASE,
+    database: 'agenda',
     host: process.env.MYSQL_HOST,
     dialect: 'mysql',
+    port: process.env.MYSQL_PORT
   },
   test: {
     username: process.env.MYSQL_USER,
     password: process.env.MYSQL_PASSWORD,
-    database: process.env.MYSQL_DATABASE,
+    database: 'agenda',
     host: process.env.MYSQL_HOST,
     dialect: 'mysql',
+    port: process.env.MYSQL_PORT
   },
   production: {
     username: process.env.MYSQL_USER,
     password: process.env.MYSQL_PASSWORD,
-    database: process.env.MYSQL_DATABASE,
+    database: 'agenda',
     host: process.env.MYSQL_HOST,
     dialect: 'mysql',
+    port: process.env.MYSQL_PORT
   },
 };
